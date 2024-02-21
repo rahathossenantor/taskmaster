deb41bed8ae64d2963e53b1320a7631bc9066a2a
mdrahathossenantor <md.rahathossenantor@gmail.com>
2024-02-22 00:09:23 +0600

    Add task functionality implemented

--- src/redux/features/tasks/tasksSlice.js
+++ src/redux/features/tasks/tasksSlice.js
@@ -7,7 +7,14 @@ const initialState = {
 const tasksSlice = createSlice({
     name: "tasks",
     initialState,
-    reducers: {}
+    reducers: {
+        addTask: (state, { payload }) => {
+            const id = state.tasks.length + 1;
+            state.tasks.push({ id, ...payload });
+        }
+    }
 });
+
+export const { addTask } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
