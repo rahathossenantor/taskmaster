979d5f151184b5658b2e96dc154362db9143350b
mdrahathossenantor <md.rahathossenantor@gmail.com>
2024-02-24 23:50:53 +0600

    Form created with React hook form for posting data

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,12 +1,25 @@
+import { useForm } from "react-hook-form";
 import { useGetPostsQuery } from "../redux/features/api/baseApi";
 
 const Chat = () => {
   const { data: posts = [] } = useGetPostsQuery();
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
 
   return (
     <div>
       <h1 className="text-center text-4xl mt-4 mb-2">Feed</h1>
       <div className="border">
+        <div className="border p-3 m-3 text-center">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex gap-3 items-center justify-center">
+            <input type="text" id="post" className="w-full rounded-xl" {...register("post")} />
+            <button className="btn bg-blue-600 text-white">Post</button>
+          </form>
+        </div>
         {
           posts?.map(post =>
             <div key={post.id} className="border m-3 p-3 text-center">
